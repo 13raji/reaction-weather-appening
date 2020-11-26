ea15bd890dd34553dec19b9c5ce48e591d101aa4
13raji <70148189+13raji@users.noreply.github.com>
2020-11-26 12:07:21 +0000

    css forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -3,25 +3,25 @@ import './Forecast.css';
 
 export default function Forecast(props){
     return(
-        <div classname="Forcast">
-            <div className="row">
-                <div className="col-2"></div>
+        <div className="Forecast-wrapper">
+            <div className="Forecast row">
+                
                 <div className="Time col-1">
                     <h6>18:00</h6>
                 </div>
-                <div className="Forecast-temp col-1">
+                <div className="Forecast-temp col-2">
                     <h4>13°C</h4>
                 </div>
                 <div className="Forecast-icon col-1">
                     <h4>☁</h4>
                 </div>
-                <div className="Feels-like col-2">
+                <div className="Feels-like col-4">
                     <h6>Feels like 12°C</h6>
                 </div>
-                <div className="col-2">
+                <div className="Forecast-precip col-4">
                     <h6>Precipitaion: 69%</h6>
                 </div>
-                <div className="col-3"></div>
+                
                 
             </div>
         </div>
