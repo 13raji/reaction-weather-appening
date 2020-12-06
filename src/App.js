3cd55d87f1f623f3ae15c124b5641127f53ada45
13raji <70148189+13raji@users.noreply.github.com>
2020-12-06 10:56:28 +0000

    removed forecast from app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import CurrentWeather from './CurrentWeather';
-import Forecast from './Forecast';
 import Footer from './Footer';
 
 export default function App() {
@@ -10,11 +9,6 @@ export default function App() {
       <div className="container">
         <div className="Weather-app">
           <CurrentWeather defaultCity="London" />
-          <Forecast />
-          <Forecast />
-          <Forecast />
-          <Forecast />
-          <Forecast />
         </div>
         <Footer />
       </div>
